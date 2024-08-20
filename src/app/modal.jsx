75ab00261698ca2a19980af3a64c792b6050dca2
UmerSaleemUltra <us8000750@gmail.com>
2024-08-21 00:39:31 +0500

    Delete Import code

--- src/app/modal.jsx
+++ src/app/modal.jsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import Modal from './'; // Import the Modal component
 
 const Modal = () => {
   const router = useRouter();
